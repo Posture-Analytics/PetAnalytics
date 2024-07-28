40a08557d3dc2a962f025c6af52597f837304e4c
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2024-07-28 11:27:47 -0300

    Create config.h file
    - store configuration parameters such as WiFi credentials and Firebase keys

---
+++ M5Stack/firebase/config.h
@@ -0,0 +1,16 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Configurações do Wi-Fi
+#define WIFI_SSID "<YOUR_WIFI_SSID>"
+#define WIFI_PASSWORD "<YOUR_WIFI_PASSWORD>"
+
+// Configurações do Firebase
+#define DATABASE_URL "<YOUR_DATABASE_URL>"
+#define DATABASE_API_KEY "<YOUR_DATABASE_API_KEY>"
+
+#define DATABASE_USER_EMAIL "<YOUR_USER_EMAIL>"
+#define DATABASE_USER_PASSWORD "<YOUR_USER_PASSWORD>"
+
+#endif // CONFIG_H
