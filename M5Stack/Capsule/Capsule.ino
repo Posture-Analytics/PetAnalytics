0c53c12083bdeddcd7ccf0b2b0a2cc9687e42838
Walleria Simoes <walleriasimoes@gmail.com>
2024-12-06 22:20:35 -0300

    Verifica continuamente a conexao com internet e com a firebase antes de coletar e enviar os dados

--- M5Stack/Capsule/Capsule.ino
+++ M5Stack/Capsule/Capsule.ino
@@ -11,7 +11,7 @@ FirebaseData firebaseData;
 
 bool imuActive = true; // variável para controlar o estado do IMU
 int imuReadingsCount = 1; // contador de leituras do IMU
-const int readingsThreshold = 100; // limite de leituras antes de enviar ao Firebase
+const int readingsThreshold = 80; // limite de leituras antes de enviar ao Firebase
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", -10800, 600);
@@ -49,6 +49,12 @@ void setup() {
     auth.user.email = DATABASE_USER_EMAIL;
     auth.user.password = DATABASE_USER_PASSWORD;
     Firebase.begin(&config, &auth);
+    if (!Firebase.ready()) {
+      Serial.println("Erro: Firebase não está pronto após inicialização");
+      while (true) {
+          delay(1000); // Mantém o ESP32 em espera para depuração
+      }
+    }
     Firebase.reconnectWiFi(true);
 
     // Cria o semáforo
@@ -56,12 +62,41 @@ void setup() {
 
     // Cria as tarefas
     xTaskCreatePinnedToCore(
-        collectIMUData, "Task1", 10000, NULL, 1, &Task1, 0);
+        collectIMUData, "Task1", 15000, NULL, 1, &Task1, 0);
     xTaskCreatePinnedToCore(
-        sendDataToFirebase, "Task2", 10000, NULL, 2, &Task2, 1);
+        sendDataToFirebase, "Task2", 15000, NULL, 2, &Task2, 1);
 
     Serial.println("Configuração concluída");
 }
+
+void checkWiFiConnection() {
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Wi-Fi desconectado. Tentando reconectar...");
+        WiFi.disconnect();
+        WiFi.reconnect();
+
+        unsigned long startAttemptTime = millis();
+        while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000) { // 10 segundos de timeout
+            delay(500);
+            Serial.print(".");
+        }
+
+        if (WiFi.status() == WL_CONNECTED) {
+            Serial.println("\nReconectado ao Wi-Fi!");
+        } else {
+            Serial.println("\nFalha ao reconectar ao Wi-Fi!");
+        }
+    }
+}
+
+void reconnectFirebase() {
+    if (!Firebase.ready()) {
+        Serial.println("Reiniciando Firebase...");
+        Firebase.reconnectWiFi(true);  // Garante que o Firebase tente reconectar o Wi-Fi
+        delay(1000);  // Pequeno atraso para estabilização
+    }
+}
+
 
 void collectIMUData(void * parameter) {
     float ax, ay, az; // Variáveis para acelerômetro
@@ -87,8 +122,8 @@ void collectIMUData(void * parameter) {
 
         // update button state
         M5.update();
-
-        if (imuActive) {
+        checkWiFiConnection();
+        if (WiFi.status() == WL_CONNECTED && imuActive) {
             // Atualiza os dados do IMU
             if (M5.Imu.update()) {
                 // Obtém os dados do acelerômetro
@@ -109,12 +144,14 @@ void collectIMUData(void * parameter) {
                     activeJsonData->add(String(imuReadingsCount), jsonEntry);
 
                     imuReadingsCount++;
-                    Serial.println(imuReadingsCount);
+                    //Serial.println(imuReadingsCount);
                 }
 
                 xSemaphoreGive(xSemaphore);
             }
-        }
+        } 
+
+        vTaskDelay(6);
 
         // Atualiza o tempo a cada segundo
         if (millis() - lastTimestamp >= 1000) {
@@ -144,17 +181,26 @@ void sendDataToFirebase(void * parameter) {
 
             xSemaphoreGive(xSemaphore);
 
-            if (sendingJsonData != nullptr) {
-              Serial.println("Enviando dados ao Firebase...");
-              if (Firebase.pushJSON(firebaseData, "/IMUData", *sendingJsonData)) {
+            if (WiFi.status() == WL_CONNECTED && Firebase.ready()) {
+              if (sendingJsonData != nullptr) {
+                Serial.println("Enviando dados ao Firebase...");
+                if (Firebase.pushJSON(firebaseData, "/IMUData", *sendingJsonData)) {
                   Serial.println("Dados enviados ao Firebase!");
                   sendingJsonData->clear();
+                } else {
+                    Serial.println("Erro ao enviar dados: " + firebaseData.errorReason());
+                    delay(500);  // Adicione um pequeno atraso para evitar loop de erros
+              } 
               } else {
-                  Serial.println("Erro ao enviar dados: " + firebaseData.errorReason());
-              }
-            } else {
                 Serial.println("Erro: sendingJsonData é null");
+                }
+              
+            } else {
+              Serial.println("Firebase ou Wi-Fi não está pronto.");
+              checkWiFiConnection();  // Tenta reconectar se necessário
+              reconnectFirebase();    // Reinicializa o Firebase se necessário
             }
+
         } else {
             xSemaphoreGive(xSemaphore);
         }
